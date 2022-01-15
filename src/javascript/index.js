5a3773c539a2b8b6c08d4430855c587d7bd5801b
StuckDuck7710 <stuck@stuckduck.it>
2022-01-15 09:37:16 +0100

    Initial commit

---
+++ src/javascript/index.js
@@ -0,0 +1,4 @@
+/* const DarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+const LightTheme = window.matchMedia("(prefers-color-scheme: light)");
+
+DarkTheme.matches ? $('#style').attr('href', "./styles/dark.css") : $('#style').attr('href', "./styles/light.css") */
