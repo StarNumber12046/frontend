9eb83da680ba2028da1a4e95b3e768fe0d35f5d2
StuckDuck7710 <stuck@stuckduck.it>
2022-01-21 22:48:57 +0100

    Added responsive navbar layout

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,4 +1,14 @@
 const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
 const lightTheme = window.matchMedia("(prefers-color-scheme: light)");
 
-darkTheme.matches ? $('#colorScheme').attr('href', "./src/styles/theme/charmga/dark.css") : $('#colorScheme').attr('href', "./src/styles/theme/charmga/light.css")
+const smallScreen = window.matchMedia("screen and (max-width: 480px)");
+
+darkTheme.matches
+? $('#colorScheme').attr('href', "./src/styles/theme/charmga/dark.css")
+: $('#colorScheme').attr('href', "./src/styles/theme/charmga/light.css")
+
+if (smallScreen.matches) {
+	$('navbarItem > span').attr(
+		'style', "display: none;"
+	)
+}
