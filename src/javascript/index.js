219517c77b482e80b1168898c94ed4013a08ea54
StuckDuck <stuck@stuckduck.it>
2022-01-21 23:29:46 +0100

    Merged fasmga/navbar-1

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,14 +1,6 @@
 const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
 const lightTheme = window.matchMedia("(prefers-color-scheme: light)");
 
-const smallScreen = window.matchMedia("screen and (max-width: 480px)");
-
 darkTheme.matches
 ? $('#colorScheme').attr('href', "./src/styles/theme/charmga/dark.css")
 : $('#colorScheme').attr('href', "./src/styles/theme/charmga/light.css")
-
-if (smallScreen.matches) {
-	$('navbarItem > span').attr(
-		'style', "display: none;"
-	)
-}
