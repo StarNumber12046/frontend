7fc904d7c82d25e2da3dff801154ac57fe49c7ad
StuckDuck7710 <stuck@stuckduck.it>
2022-01-22 16:22:49 +0100

    Testing MOTDs
    Co-authored-by: ParliamoDiPC <66361695+ParliamoDiPC@users.noreply.github.com>

--- src/javascript/pageload.js
+++ src/javascript/pageload.js
@@ -15,7 +15,36 @@
 /*** —————————————————— ***//*** —————————————————— ***//*** —————————————————— ***//*** —————————————————— ***/
 
 /***
-	Replace the '#navigation' object, present in most pages, with the
-	standardized navbar in "./content/std/navbar.html", to avoid repetition.
+	Replace the '#navigation' div, present in most pages, with the
+	standardized navbar in "/content/std/navbar.html", to avoid repetition.
 ***/
 $("#navigation").load("./content/std/navbar.html");
+
+/***
+	Shows the message of the day in the '#motd' info panel picked randomly from
+	"/assets/motd.json".
+***/
+$.getJSON("./assets/motd.json", (data) => {
+	// Generates a random floating point number between 0 and 1
+	const randint = Math.random();
+	// Stringify the result
+	const randstr = JSON.stringify(randint);
+	let randomMotd = randstr.split('.')[1]
+	.slice(
+		-1 * (
+			// Get the length of the stringified length of the array
+			// In other words if the array contains 12 elements, we get 2 because '12'
+			// is 2 characters long
+			// This is so that we can get a random number that's actually in the
+			// range of our array
+			JSON.stringify(data.length).length
+		)
+	); // At this point we got the first [JSON.stringify(data.length).length]
+	// digits after the decimal point
+	randomMotd = int(randomMotd);
+
+	// Finally, get a result
+	const motd = data[randomMotd];
+	// Write it in
+	$('#motd').attr('innerText', motd)
+})
