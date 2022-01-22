4f853bfa2edc3f9cdbbc2baf141c6f9def1a9516
StuckDuck7710 <stuck@stuckduck.it>
2022-01-22 11:08:46 +0100

    Finalized components

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,5 +2,5 @@ const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
 const lightTheme = window.matchMedia("(prefers-color-scheme: light)");
 
 darkTheme.matches
-? $('#colorScheme').attr('href', "./src/styles/theme/charmga/dark.css")
-: $('#colorScheme').attr('href', "./src/styles/theme/charmga/light.css")
+? $('#colorScheme').attr('href', "./src/css/theme/charmga/dark.css")
+: $('#colorScheme').attr('href', "./src/css/theme/charmga/light.css")
