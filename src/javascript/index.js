67fe74d28c0a81c96371d54940299c3b4f65ec11
StuckDuck <stuck@stuckduck.it>
2022-01-22 19:33:25 +0100

    Testing link cards
    * updated `index.html` && `index.js`

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -4,3 +4,17 @@ const lightTheme = window.matchMedia("(prefers-color-scheme: light)");
 darkTheme.matches
 ? $('#colorScheme').attr('href', "./src/css/theme/charmga/dark.css")
 : $('#colorScheme').attr('href', "./src/css/theme/charmga/light.css")
+
+$('#newLink').on('submit', (event) => {
+	event.preventDefault();
+	const url = $('#urlBox').attr('value');
+	let ogData = $.get(url);
+
+	if (ogData) {
+		console.log(ogData);
+		return true;
+	}
+
+	console.log("failed");
+	return false;
+});
