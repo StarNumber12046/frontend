6ba894afc37f48776525f5df51fb0cfff351ce25
StuckDuck7710 <stuck@stuckduck.it>
2022-01-22 15:36:03 +0100

    Fixed formatting and added intros
    Co-authored-by: ParliamoDiPC <ParliamoDiPC@users.noreply.github.com>

--- src/javascript/pageload.js
+++ src/javascript/pageload.js
@@ -1,1 +1,21 @@
+/*
+		 _____
+		|  ___|_ _ ___ _ __ ___    __ _  __ _
+		| |_ / _` / __| '_ ` _ \  / _` |/ _` |
+		|  _| (_| \__ \ | | | | || (_| | (_| |
+		|_|  \__,_|___/_| |_| |_(_)__, |\__,_|
+															|___/
+	
+	-> Name: pageload.js
+	-> Description: Executed at startup for data binding
+	-> Resource: /src/javascript/pageload.js
+	-> Licensing: © 2022, Fasm.ga | https://www.fasmga.org/license
+*/
+
+/*** —————————————————— ***//*** —————————————————— ***//*** —————————————————— ***//*** —————————————————— ***/
+
+/***
+	Replace the '#navigation' object, present in most pages, with the
+	standardized navbar in "./content/std/navbar.html", to avoid repetition.
+***/
 $("#navigation").load("./content/std/navbar.html");
