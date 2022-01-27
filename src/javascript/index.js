936ad7d796298a07320dd5d440c9d09d8b7c866a
StuckDuck7710 <stuck@stuckduck.it>
2022-01-27 13:50:16 +0100

    Removed unused scripts

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -13,27 +13,3 @@
 */
 
 /*** —————————————————— ***//*** —————————————————— ***//*** —————————————————— ***//*** —————————————————— ***/
-
-/***
-	Handling data from the '#newLink' form
-***/
-$('#newLink').on('submit', (event) => {
-	// Prevent reloading the page
-	event.preventDefault();
-	// Get the current value inserted in the URL box
-	const url = $('#urlBox').attr('value');
-	// Get the webpage metadata from url
-	let ogData = $.get(url);
-
-	// If ogData is not null, return the OpenGraph Protocol data
-	// used to extract link previews
-	if (ogData) {
-		console.log(ogData);
-		return true;
-	}
-
-	// Else, return false and fallback to the default link card
-	// style
-	console.log("failed");
-	return false;
-});
