bbb4c79b0a000f76c55a8668fadd876dc57579db
StuckDuck7710 <stuck@stuckduck.it>
2022-01-27 13:29:20 +0100

    Moved dynamic theming to `pageload.html`
    Co-authored-by: ParliamoDiPC <ParliamoDiPC@users.noreply.github.com>

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -15,24 +15,6 @@
 /*** —————————————————— ***//*** —————————————————— ***//*** —————————————————— ***//*** —————————————————— ***/
 
 /***
-	Dynamic theming based on user's system preferences
-***/
-// Let's check if the user likes light theme through the browser
-let prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
-
-// (These are the only currently available themes)
-const darkTheme = "./src/css/theme/charmga/dark.css";
-const lightTheme = "./src/css/theme/charmga/light.css";
-
-// If the user prefers light theme we'll apply light theme to the webpage
-// Else fall back to the default dark theme
-let theme = prefersLight ? lightTheme : darkTheme;
-
-// We set the '#colorScheme' link to our user's preferred theme
-// this is because CSS doesn't allow for dynamic imports with media queries
-$('#colorScheme').attr('href', theme)
-
-/***
 	Handling data from the '#newLink' form
 ***/
 $('#newLink').on('submit', (event) => {
--- src/javascript/pageload.js
+++ src/javascript/pageload.js
@@ -14,5 +14,32 @@
 
 /*** —————————————————— ***//*** —————————————————— ***//*** —————————————————— ***//*** —————————————————— ***/
 
-window.location.hash = "#newLink"; // Shows the '#newLink' section by default
-$("#navigation").load("/content/navbar.html");
+/***
+	Dynamic theming based on user's system preferences
+***/
+// Let's check if the user likes light theme through the browser
+let prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
+
+// (These are the only currently available themes)
+const darkTheme = "./src/css/theme/charmga/dark.css";
+const lightTheme = "./src/css/theme/charmga/light.css";
+
+// If the user prefers light theme we'll apply light theme to the webpage
+// Else fall back to the default dark theme
+let theme = prefersLight ? lightTheme : darkTheme;
+
+// We set the '#colorScheme' link to our user's preferred theme
+// this is because CSS doesn't allow for dynamic imports with media queries
+$('#colorScheme').attr('href', theme)
+
+/***
+	Automatically move to the user's preferred home page
+***/
+let home = "#newLink"; // #newLink is the default
+window.location.hash = home; // automatically moves to the section in [home]
+
+/***
+	Load the global website navigation bar
+***/
+let navbar = "/content/navbar.html"; // has to be an HTML file
+$("#navigation").load(navbar);
