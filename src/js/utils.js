0f90deb6395d1d5052c9460aff03d4d1b0981c3d
StuckDuck7710 <stuck@stuckduck.it>
2022-01-30 13:36:45 +0100

    Incremented version

--- src/js/utils.js
+++ src/js/utils.js
@@ -20,8 +20,8 @@
 ***/
 function setTheme(resource) {
 	switch (resource) {
-		case 'dark': resource = "./src/css/theme/charmga/dark.css?v=2b41b71"; break;
-		case 'light': resource = "./src/css/theme/charmga/light.css?v=2b41b71"; break;
+		case 'dark': resource = "./src/css/theme/charmga/dark.css?v=2b41b73"; break;
+		case 'light': resource = "./src/css/theme/charmga/light.css?v=2b41b73"; break;
 	}
 	$('#colorscheme').attr('href', resource)
 }
